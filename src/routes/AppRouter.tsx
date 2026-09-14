cfd7dd202215da409845d24f003d21d8dd13893f
Diego Caceres <caceresdiegoleonel@gmail.com>
2026-09-14 17:35:20 -0300

    feat(router): agregar AppRouter con rutas protegidas

---
+++ src/routes/AppRouter.tsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Home from "../pages/Home";
+import LoginPage from "../pages/Login";
+import RegisterPage from "../pages/Register";
+import TasksPage from "../pages/Tasks";
+import { ProtectedRoute } from "./ProtectedRoute";
+
+export const AppRouter = () => (
+    <BrowserRouter>
+        <Navbar />
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route
+                path="/tasks"
+                element={
+                    <ProtectedRoute>
+                        <TasksPage />
+                    </ProtectedRoute>
+                }
+            />
+        </Routes>
+    </BrowserRouter>
+);
