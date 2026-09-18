660a37ee746b02769adcfefb7359b89033e85323
Diego Caceres <caceresdiegoleonel@gmail.com>
2026-09-18 17:44:11 -0300

    feat(firebase): se crea y exporta googleProvider

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Las credenciales se leen desde variables de entorno
@@ -18,3 +18,4 @@ const app = initializeApp(firebaseConfig);
 // Exportar servicios
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const googleProvider = new GoogleAuthProvider();
